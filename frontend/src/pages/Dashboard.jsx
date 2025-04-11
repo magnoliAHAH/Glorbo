e8b7b6c0f0a8c9b9d8fec87880a2e1df0d09242f
magnoliAHAH <mixail.ermin33@gmail.com>
2025-04-11 18:19:39 +0300

    fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,7 @@ import Graph from '../components/Graph'
 const Dashboard = () => {
   const [structure, setStructure] = useState(null)
   useEffect(() => {
-    fetch('http://localhost:8080/api/structure?repo=https://github.com/magnoliAHAH/roulette')
+    fetch('http://supreme-roulette.work.gd/api/structure?repo=https://github.com/magnoliAHAH/roulette')
       .then(res => res.json())
       .then(data => setStructure(data))
   }, [])
