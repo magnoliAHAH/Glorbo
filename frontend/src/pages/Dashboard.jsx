e4cd065ca9b88d604be255c4aec2a86ed9e26a92
magnoliAHAH <mixail.ermin33@gmail.com>
2025-04-23 21:37:11 +0300

    navigate to dashboard with local fix

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,29 +1,28 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useHistory } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Graph from '../components/Graph';
 
 const Dashboard = () => {
   const [structure, setStructure] = useState(null);
   const location = useLocation();
-  const history = useHistory();
+  const navigate = useNavigate();
 
   const params = new URLSearchParams(location.search);
   const urlRepo = params.get('repo');
 
   useEffect(() => {
-    // Сначала проверим, если repo передано через URL
     const repo = urlRepo || localStorage.getItem('repo');
 
     if (!repo) {
-      history.push('/projects');
+      navigate('/projects');
       return;
     }
-    
+
     fetch(`https://supreme-roulette.work.gd/api/structure?repo=${repo}`)
       .then(res => res.json())
       .then(data => setStructure(data))
-      .catch((err) => console.error('Error fetching structure:', err));
-  }, [urlRepo, history]);
+      .catch(err => console.error('Error fetching structure:', err));
+  }, [urlRepo, navigate]);
 
   return (
     <div style={{ height: '80vh', width: '100%' }}>
