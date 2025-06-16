624944191dfe108bf66abd61d3e34c1d57a712be
magnoliAHAH <mixail.ermin33@gmail.com>
2025-06-16 23:30:48 +0300

    add API endpoint for list services try 12

--- frontend/src/components/DashboardForMain.jsx
+++ frontend/src/components/DashboardForMain.jsx
@@ -13,7 +13,7 @@ import ReactFlow, {
 import 'reactflow/dist/style.css';
 
 // Импорты API и утилит
-import { createService, updateNodePosition, getRepoTree, createAuthService } from '../functions/api/api';
+import { createService, updateNodePosition, getRepoTree, createAuthService, getProjectServices } from '../functions/api/api';
 import { createReactFlowServiceNode, renderFileNodeForSidebar, renderServiceInfoForSidebar, convertFileNodeToReactFlowElements } from '../functions/utils';
 
 // --- Styled Components --- (без изменений)
